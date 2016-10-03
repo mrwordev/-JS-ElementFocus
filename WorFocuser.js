63ead546710e0885573d9798d7290f7eaed1e65f
Woraprat Jindain <mark2396@hotmail.com>
2016-10-03 16:23:12 +0700

    Create functions and methods

---
+++ WorFocuser.js
@@ -0,0 +1,66 @@
+var WorFocuser = (function(document, window) {
+    var $ = {
+        target: null,
+        overlay: null,
+        init: function() {
+            $.overlay = createOverlay();
+        },
+        createOverlay: function() {
+            var check = document.getElementById('WorOverley');
+            if (check !== null) {
+                return;
+            }
+            var overlay = document.createElement('div');
+            overlay.id = "WorOverley";
+            overlay.style.position = 'absolute';
+            overlay.style.zIndex = "999999";
+            overlay.style.boxShadow = "0 0 0 3000px rgba(0,0,0,0.8)";
+            overlay.style.pointerEvents = "none";
+            overlay.style.display = "none";
+            document.getElementsByTagName('body')[0].appendChild(overlay);
+            return overlay;
+        },
+        relocation: function(e) {
+            if ($.target === null) {
+                if (e === null || e === undefined) {
+                    return;
+                } else {
+                    $.target = e;
+                }
+            }
+            var height = $.target.offsetHeight;
+            var width = $.target.offsetWidth;
+            var top = $.target.offsetTop;
+            var left = $.target.offsetLeft;
+            $.overlay.style.height = height;
+            $.overlay.style.width = width;
+            $.overlay.style.top = top;
+            $.overlay.style.left = left;
+            $.overlay.style.display = "block";
+            window.addEventListener("resize", function($) {
+                return function() {
+                    console.log("resize");
+                    $.relocation();
+                }
+            }($));
+        },
+        toggle: function(e) {
+            var element = document.getElementById(e);
+            if (element === null) {
+                return;
+            }
+            if ($.overlay === null) {
+                $.overlay = $.createOverlay();
+            }
+            $.relocation(element);
+        },
+        lostFocus: function() {
+            var overlay = document.getElementById('WorOverley');
+            if (overlay === null) {
+                return;
+            }
+            overlay.style.display = "none";
+        }
+    }
+    return $;
+})(document, window);
